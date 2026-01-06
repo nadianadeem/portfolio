5487a4f6ed26c8661308c184ed38a541ceb148ab
Nadia Nadeem <nadianadeem365@gmail.com>
2026-01-06 19:37:06 +0000

    Remove cache (I want the most up to date contributions.

--- features/github/server/get-contributions.ts
+++ features/github/server/get-contributions.ts
@@ -6,14 +6,10 @@ type GitHubContributionsResponse = {
   contributions: Activity[];
 };
 
-export const getGitHubContributions = unstable_cache(
-  async () => {
-    const res = await fetch(
-      `https://github-contributions-api.jogruber.de/v4/nadianadeem?y=last`,
-    );
-    const data = (await res.json()) as GitHubContributionsResponse;
-    return data.contributions;
-  },
-  ["github-contributions"],
-  { revalidate: 86400 }, // Cache for 1 day (86400 seconds)
-);
+export const getGitHubContributions = unstable_cache(async () => {
+  const res = await fetch(
+    `https://github-contributions-api.jogruber.de/v4/nadianadeem?y=last`,
+  );
+  const data = (await res.json()) as GitHubContributionsResponse;
+  return data.contributions;
+}, ["github-contributions"]);
